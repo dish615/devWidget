156a8d42e2b766d31c3bd480a01b95b0cb490191
Disha <disha@thetacboard.com>
2016-03-01 18:30:50 -0500

    HELP ME IM STUCK IN CODE

--- check.js
+++ check.js
@@ -17,7 +17,7 @@ important.setAttribute('size', important.getAttribute('placeholder').length);
     
    
     
-    $(".tacboard-form").keypress(function(){
+    $(".tacboard-form").keypress(function(event){
         return event.keyCode != 13;
     });
     
@@ -32,13 +32,80 @@ important.setAttribute('size', important.getAttribute('placeholder').length);
                     console.log($("#invisible").val());
                 var y= $("#invisible").val();
                 imp.click();
-                window.open('http://localhost:3000/signup-pop?price='+x+"&url="+encodeURIComponent(y), 'newwindow', 'width=600, height=450'); 
+                window.open('http://localhost:3000/signup-pop?price='+x+'&url='+encodeURIComponent(y), 'newwindow', 'width=600, height=450'); 
                 imp.setAttribute("type", "button");
                 //document.location.replace("/");
                 return false;
             }  
 
     });
+    
+
+/*keep $ symbol present!*/
+    
+var readOnlyLength = 2;
+
+
+ 
+ $("#tac-input").focus(function(){
+	$(this).val("$ ");
+ });
+ 
+ $("#tac-input").blur(function(){
+ $(this).val("");
+ $(this).attr("placeholder", "$ PRICE I'D PAY");
+ 
+ });
+ 
+ 
+
+$("#tac-input").on("click", function(){
+	
+	
+		if(this.selectionStart< readOnlyLength) {
+    	
+    	this.selectionEnd= 3;
+    }
+});
+
+
+
+
+$("#tac-input").on("keypress, keydown", function(e){
+
+	if((event.which== 39) && ($("#tac-input").val()== "$ ")) {
+  	this.selectionStart= 3;
+  }
+});
+
+$('#tac-input').on('keypress, keydown', function(event) {
+    
+    
+    if(event.which== 38) {
+    return false;
+    }
+    
+    
+    
+    if(((event.which== 8) || (event.which== 37)) && ((this.selectionStart< readOnlyLength) || (this.selectionStart== readOnlyLength))) {
+    	console.log(this.selectionStart, this.selectionEnd);
+      var start= this.selectionStart;
+      var end= this.selectionEnd;
+      var len= $("#tac-input").val().length;
+      if((event.which== 8) && (end-start===len)) {
+      	
+        
+        $("#tac-input").val("$  ");
+      } else {
+      	return false;
+      }
+    	
+    }
+    
+ 
+});                    
+    
+    
     
 
     
--- tac-widget.js
+++ tac-widget.js
@@ -42,7 +42,7 @@ function main() {
         var css_link = $("<link>", { 
             rel: "stylesheet", 
             type: "text/css", 
-            href: "https://cdn.rawgit.com/dish615/widgetAwesome/master/style.css" 
+            href: "https://cdn.rawgit.com/dish615/widgetbest/master/style.css" 
 
         });
        
@@ -61,14 +61,14 @@ function main() {
        var widg_script = $("<script>", {
            type: "text/javascript",
             async: true,
-            src: "https://cdn.rawgit.com/dish615/widgetAwesome/master/check.js"
+            src: "https://cdn.rawgit.com/dish615/widgetbest/master/check.js"
         });
         
         widg_script.appendTo("head");
         
 
         
-        var jsonp_url= "https://cdn.rawgit.com/dish615/widgetAwesome/master/index.html";
+        var jsonp_url= "https://cdn.rawgit.com/dish615/widgetbest/master/index.html";
         $.get(jsonp_url, function(data){
             console.log(data);
             console.log(typeof data);
