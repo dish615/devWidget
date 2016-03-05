885ee374e147b879b9de4742883c6fa13a637887
Disha <disha@thetacboard.com>
2016-03-05 00:06:35 -0500

    fdsafdsa

--- tac-widget.js
+++ tac-widget.js
@@ -42,7 +42,7 @@ function main() {
         var css_link = $("<link>", { 
             rel: "stylesheet", 
             type: "text/css", 
-            href: "https://cdn.rawgit.com/dish615/Widget200/master/style.css" 
+            href: "https://cdn.rawgit.com/dish615/Widget400/master/style.css" 
 
         });
        
@@ -61,14 +61,14 @@ function main() {
        var widg_script = $("<script>", {
            type: "text/javascript",
             async: true,
-            src: "https://cdn.rawgit.com/dish615/Widget200/master/check.js"
+            src: "https://cdn.rawgit.com/dish615/Widget400/master/check.js"
         });
         
         widg_script.prependTo("head");
         
 
         
-        var jsonp_url= "https://cdn.rawgit.com/dish615/Widget200/master/index.html";
+        var jsonp_url= "https://cdn.rawgit.com/dish615/Widget400/master/index.html";
         $.get(jsonp_url, function(data){
             console.log(data);
             console.log(typeof data);
