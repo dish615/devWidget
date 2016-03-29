3cb9b9034fdf411124260c8d3aab5753f5eea248
Disha <disha@thetacboard.com>
2016-03-29 01:13:31 -0400

    changed to heroku

--- check.js
+++ check.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
         window.addEventListener("message", function(e){
                 console.log(e.origin);
                 console.log(e.data);
-            if(e.origin!=="https://localhost:2000") {
+            if(e.origin!=="https://thesavyapp.herokuapp.com") {
                 return;
             } else {
                 var position= e.data;
