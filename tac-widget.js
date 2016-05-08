5f412d9103f8e5e107c2c21bb510e3fd3e0d7224
Disha <disha@thetacboard.com>
2016-05-08 14:38:44 -0400

    ready for dev

--- tac-widget.js
+++ tac-widget.js
@@ -39,7 +39,7 @@ function scriptLoadHandler() {
 /******** Our main function ********/
 function main() { 
     jQuery(document).ready(function($) { 
-        var web= "https://staysavy.com";
+        var web= "https://staging-thesavyapp.herokuapp.com";
         var btn;
         /*
         var htmlCSP='<meta http-equiv="Content-Security-Policy" content="default-src *; style-src * \'unsafe-inline\'; script-src * \'unsafe-inline\' \'unsafe-eval\'; img-src * data: \'unsafe-inline\'; connect-src * \'unsafe-inline\'; child-src *; frame-src *">';
@@ -82,7 +82,7 @@ function main() {
         var css_link = $("<link>", { 
             rel: "stylesheet", 
             type: "text/css", 
-            href: "https://cdn.rawgit.com/dish615/Widget136/master/style.css" 
+            href: "https://cdn.rawgit.com/dish615/devWidget26/master/style.css" 
 
         });
        
@@ -101,14 +101,14 @@ function main() {
        var widg_script = $("<script>", {
            type: "text/javascript",
             async: true,
-            src: "https://cdn.rawgit.com/dish615/Widget136/master/check.js"
+            src: "https://cdn.rawgit.com/dish615/devWidget26/master/check.js"
         });
         
         widg_script.prependTo("head");
         
 
         
-        var jsonp_url= "https://cdn.rawgit.com/dish615/Widget136/master/index.html";
+        var jsonp_url= "https://cdn.rawgit.com/dish615/devWidget26/master/index.html";
         $.get(jsonp_url, function(data){
             console.log(data);
             console.log(typeof data);
