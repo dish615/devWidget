f63815beb6a1a2b905b78ec6abe3bd4e9369db91
Disha <disha@thetacboard.com>
2016-05-12 22:55:36 -0400

    ready for prod

--- check.js
+++ check.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //dont think this is working...
-    var web= "https://staging-thesavyapp.herokuapp.com";
+    var web= "https://staysavy.com";
     var socket = io.connect(web);
 /*
     if(tbCookieExists()) {
